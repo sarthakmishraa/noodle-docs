7c2ff2b46e97dbdc8da4b8ed3f6d48320e357261
Sarthak Mishra <msarthak785@gmail.com>
2024-07-18 18:45:16 -0400

    minor changes

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "*",
+        origin: "https://noodledocs.vercel.app/",
         methods: ["GET", "POST"]
     }
 });
